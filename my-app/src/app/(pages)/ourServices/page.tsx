9f6823a4221ba2980c1c116270073c992528d226
Ano2225 <ouatt0767@gmail.com>
2024-10-30 22:52:08 +0000

    updated

--- my-app/src/app/(pages)/ourServices/page.tsx
+++ my-app/src/app/(pages)/ourServices/page.tsx
@@ -7,8 +7,8 @@ import { twMerge } from 'tailwind-merge';
 import CircleTrans from '@/app/sections/circleTrans'
 import exchange from "@/app/assets/exchangee.webp";
 import binance from "@/app/assets/Binance.png";
-import sendMoney from "../../assets/Binance.png";
-import Send from "../../assets/Binance.png";
+import sendMoney from "../../assets/img1.png";
+import Send from "../../assets/img2.png";
 import { Header } from "@/app/sections/Header";
 import Footer from "@/app/sections/Footer";
 import { FaArrowRight } from "react-icons/fa6";
