25d0f08dc2aec54cd7d545955169d29a2eb51234
Ano2225 <ouatt0767@gmail.com>
2024-11-03 20:25:35 +0000

    updated OurServices Page

--- my-app/src/app/(pages)/ourServices/page.tsx
+++ my-app/src/app/(pages)/ourServices/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Image from "next/image";
 import { motion } from "framer-motion";
-import exchange from "@/app/assets/transfimg.png";
+import exchange from "@/app/assets/transfimg.svg";
 import binance from "@/app/assets/bnb.svg";
 import sendMoney from "../../assets/img-1.png";
 import Send from "../../assets/img-2.png";
