aedb938f6dfb52b5ded34cf70f1ff095fc42e899
Ano2225 <ouatt0767@gmail.com>
2024-11-05 16:38:17 +0000

    Updated NosMoyensDePaiments.tsx

--- my-app/src/app/sections/circleTrans.tsx
+++ my-app/src/app/sections/circleTrans.tsx
@@ -6,21 +6,22 @@ import logoEnter from '@/app/assets/winiLogo.png'
 function CircleTrans() {
     return (
         <div className="container mx-auto px-4">
-            <div className="flex justify-center lg:justify-end xl:max-w-4xl my-8 lg:my-12">
+            <div className="flex justify-center lg:justify-end xl:max-w-4xl my-6 lg:my-8">
                 {/* Cercle principal avec position relative pour servir de référence */}
-                <div className="relative w-full max-w-[14rem] sm:max-w-[16rem] md:max-w-[22rem] aspect-square">
+                <div className="relative w-full max-w-[14rem] sm:max-w-[16rem] md:max-w-[18rem] aspect-square">
                     {/* Cercle externe */}
-                    <div className="absolute inset-0 border border-yellow-500 rounded-full">
+                    <div className="absolute inset-0 border-2 border-yellow-500 rounded-full">
                         {/* Cercle du milieu */}
-                        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3/4 aspect-square border border-yellow-500 rounded-full">
-                            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-2/3 aspect-square bg-white rounded-full p-1 sm:p-2">
+                        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3/4 aspect-square border-2 border-yellow-500 rounded-full">
+                            {/* Cercle central avec le logo */}
+                            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-2/3 aspect-square bg-white rounded-full p-1.5 sm:p-2">
                                 <div className="relative w-full h-full flex items-center justify-center rounded-full">
                                     <Image
                                         src={logoEnter}
-                                        width={78}
-                                        height={78}
+                                        width={85}
+                                        height={85}
                                         alt="Winichange Logo"
-                                        className="w-4/5 sm:w-full h-4/5 sm:h-full object-contain"
+                                        className="w-4/5 h-4/5 object-contain"
                                     />
                                 </div>
                             </div>
@@ -29,7 +30,7 @@ function CircleTrans() {
 
                     {/* Effet de gradient */}
                     <div className="absolute inset-0 flex justify-center items-center">
-                     <div className="absolute w-80 h-80 bg-gradient-to-r from-green-500 via-teal-500 to-green-400 rounded-full opacity-30 blur-lg"></div>
+                        <div className="absolute w-full h-full bg-gradient-to-r from-green-500 via-teal-500 to-green-400 rounded-full opacity-30 blur-lg"></div>
                     </div>
 
                     {/* Apps en rotation - avec calcul relatif */}
@@ -37,12 +38,9 @@ function CircleTrans() {
                         <ul className="relative w-full h-full">
                             {winichangeApps.map((app, index) => {
                                 const angle = index * (360 / winichangeApps.length);
-                                // Convertir l'angle en radians
                                 const radian = (angle * Math.PI) / 180;
-                                // Calculer le rayon en pourcentage de la taille du parent
                                 const radius = '45%';
                                 
-                                // Calculer les positions X et Y en pourcentage
                                 const x = `${50 + 45 * Math.cos(radian)}%`;
                                 const y = `${50 + 45 * Math.sin(radian)}%`;
                                 
@@ -56,7 +54,7 @@ function CircleTrans() {
                                             transform: 'translate(-50%, -50%)',
                                         }}
                                     >
-                                        <div className="flex items-center justify-center w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 bg-white border border-gray-200/15 rounded-xl">
+                                        <div className="flex items-center justify-center w-9 h-9 sm:w-10 sm:h-10 md:w-11 md:h-11 bg-white border border-gray-200/15 rounded-xl shadow-lg">
                                             <Image
                                                 className="w-6 h-6 sm:w-7 sm:h-7 md:w-8 md:h-8 object-contain"
                                                 width={app.width}
--- my-app/src/constants/nosMoyensPaiements.ts
+++ my-app/src/constants/nosMoyensPaiements.ts
@@ -10,7 +10,7 @@ import OrangeCI from '@/app/assets/logoMobileMoney/Orange/CI.png'
 //import OrangeCM from '@/app/assets/logoMobileMoney/Orange/CM.png'
 //import OrangeMali from '@/app/assets/logoMobileMoney/Orange/MALI.png'
 import Orange from '@/app/assets/Money/om2.png'
-import Tmoney from '@/app/assets/Money/tmoney-1.png'
+import Tmoney from '@/app/assets/t-money.png'
 import PayerUSD from '@/app/assets/logoMobileMoney/Payeer/USD.png'
 import PerfectMoneyEuro from '@/app/assets/logoMobileMoney/PM/EURO.png'
 import Airtel from '@/app/assets/Money/airtel.png'
@@ -19,21 +19,21 @@ import Airtel from '@/app/assets/Money/airtel.png'
 
 
 export const  winichangeApps = [
-    { id: "1", title: "Safaricom", icon: Safaricom, width: 48, height:48 },
-    //{ id: "4", title: "Moov Togo", icon: MoovTogo, width: 48, height:48 },
-    { id: "5", title: "Free Money", icon: FreeMoney, width: 48, height:48 },
-    //{ id: "7", title: "Orange Burkina Faso", icon: OrangeBF, width: 48, height:48 },
-    // { id: "8", title: "Orange Côte d'Ivoire", icon: OrangeCI, width: 48, height:48 },
-    { id: "3", title: "Wave", icon: Wave, width: 48, height:48 },
-    //{ id: "9", title: "Orange Cameroun", icon: OrangeCM, width: 48, height:48 },
-    { id: "6", title: "MTN", icon: Mtn, width: 48, height:48 },
-    { id: "17", title: "Airtel Money", icon: Airtel, width: 48, height: 48 },
-   // { id: "10", title: "Orange Mali", icon: OrangeMali, width: 48, height:48 },
-    { id: "11", title: "Orange", icon: Orange, width: 48, height:48 },
-   // { id: "12", title: "Wave Côte d'Ivoire", icon: WaveCI, width:48, height:48 },
-   // { id: "13", title: "Wave Senegal", icon: WaveSenegal, width:48, height:48 },
-    { id: "14", title: "Tmoney", icon: Tmoney, width: 48, height: 48 },
-    // { id: "16", title: "Perfect Money Euro", icon: PerfectMoneyEuro, width: 48, height: 48 },
-    // { id: "15", title: "Payeer USD", icon: PayerUSD, width: 48, height: 48 },
-    { id: "2", title: "Moov Côte d'Ivoire", icon: MoovCI, width: 48, height:48 },
+    { id: "1", title: "Safaricom", icon: Safaricom, width: 80, height:80 },
+    //{ id: "4", title: "Moov Togo", icon: MoovTogo, width: 80, height:80 },
+    { id: "5", title: "Free Money", icon: FreeMoney, width: 80, height:80 },
+    //{ id: "7", title: "Orange Burkina Faso", icon: OrangeBF, width: 80, height:80 },
+    // { id: "8", title: "Orange Côte d'Ivoire", icon: OrangeCI, width: 80, height:80 },
+    { id: "3", title: "Wave", icon: Wave, width: 80, height:80 },
+    //{ id: "9", title: "Orange Cameroun", icon: OrangeCM, width: 80, height:80 },
+    { id: "6", title: "MTN", icon: Mtn, width: 80, height:80 },
+    { id: "17", title: "Airtel Money", icon: Airtel, width: 80, height: 80 },
+   // { id: "10", title: "Orange Mali", icon: OrangeMali, width: 80, height:80 },
+    { id: "11", title: "Orange", icon: Orange, width: 80, height:80 },
+   // { id: "12", title: "Wave Côte d'Ivoire", icon: WaveCI, width:80, height:80 },
+   // { id: "13", title: "Wave Senegal", icon: WaveSenegal, width:80, height:80 },
+    { id: "14", title: "Tmoney", icon: Tmoney, width: 80, height: 80 },
+    // { id: "16", title: "Perfect Money Euro", icon: PerfectMoneyEuro, width: 80, height: 80 },
+    // { id: "15", title: "Payeer USD", icon: PayerUSD, width: 80, height: 80 },
+    { id: "2", title: "Moov Côte d'Ivoire", icon: MoovCI, width: 80, height:80 },
 ];
