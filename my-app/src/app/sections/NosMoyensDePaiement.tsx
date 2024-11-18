56042725b97f6781f192d608adb630e95807011a
Ano2225 <ouatt0767@gmail.com>
2024-11-18 16:19:46 +0000

    Update NosMpoyensDePaiments

--- my-app/src/app/sections/NosMoyensDePaiement.tsx
+++ my-app/src/app/sections/NosMoyensDePaiement.tsx
@@ -6,13 +6,13 @@ import CircleTrans from './circleTrans';
 
 const MoyensDePaiement = () => {
   return (
-    <div className="bg-gradient-to-b from-white to-gray-50 pt-20 md:pt-0">
+    <div className="bg-gradient-to-b from-white to-gray-50 pt-10 md:pt-0">
       <div className="lg:container mx-auto px-4 md:py-24" id="countrysection">
         <div className="flex flex-col md:flex-row items-center justify-center md:p-8">
           <div className="w-full md:w-1/2 flex items-center justify-center md:mb-0">
             <div className="text-center md:text-start">
-              <h3 className="text-2xl md:text-3xl text-gray-900 font-bold mb-4 md:mb-8">
-                Plus de <span className="font-bold text-[#126e51]">40 Moyens de paiement</span>
+              <h3 className="text-xl md:text-3xl text-gray-900 font-bold mb-8">
+                Plus de <span className="font-bold text-[#126e51] ">40 Moyens de paiement</span>
                 <div className="mt-4 flex justify-center gap-2">
                     <div className="h-1 w-16 bg-[#126e51] rounded-full"></div>
                     <div className="h-1 w-4 bg-[#126e51]/60 rounded-full"></div>
